e7be61506ee5b012f0cfb20872c5f5a24c1a205f
bojandamchevski <bojandamcevski@gmail.com>
2021-01-15 23:48:09 +0100

    Update task 3

--- Homework #4 JavaSript Бојан Дамчевски/Script/homework_4_task_3.js
+++ Homework #4 JavaSript Бојан Дамчевски/Script/homework_4_task_3.js
@@ -8,3 +8,42 @@ function bigString(input) {
     }
 }
 bigString(textArray);
+
+// ---------------MORE AND BETTER WAYS---------------------
+
+// let arrayString = ["Hello", " from", " SEDC.", " Add more text if you want"];
+
+// function oneString1(input) {
+//     let emptyString = "";
+//     for (let i = 0; i < input.length; i++) {
+//         emptyString += input[i]
+//     }
+//     console.log(emptyString);
+// }
+
+// oneString1(arrayString);
+
+// function oneString2(input) {
+//     let emptyString = "";
+//     let i = 0;
+//     while (i < input.length) {
+//         emptyString += input[i]
+//         i++;
+//     }
+//     return emptyString;
+// }
+
+// var result = oneString2(arrayString);
+// console.log(`The string is ${result}`);
+
+// function oneString3(input) {
+//     let emptyString = "";
+//     for (let i of input){
+//         emptyString += i;
+//         i++;
+//     }
+//     return emptyString;
+// }
+
+// let result1 = oneString3(arrayString);
+// console.log(`The string is: ${result1}`);
